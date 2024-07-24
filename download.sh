7c7b251c2cf5f25d1fd6167eb9f61d0a0c8002c3
thesmartwon <thesmartwon@pm.me>
2024-07-24 16:09:44 -0400

    only extract .xml files to prevent overwriting repo files

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -s 'https://www.tanach.us/Books/Tanach.xml.zip' > Tanach.zip
-unzip Tanach.zip "Books/*"
+unzip Tanach.zip "Books/*.xml"
 mv Books/*.xml .
 rm -rf Books Tanach* *.DH.xml
